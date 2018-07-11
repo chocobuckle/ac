3c543bc2ca7e374614f554382ac7279f534baa04
Conor Doyle <contact@conordoyle.io>
2018-07-11 11:32:43 +0100

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Aerochamber Plus Flow-Vu | Anti-Static Valved Holding Chamber',
+    desc: 'Anti-Static Valved Holding Chamber'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'img',
+        path: `${__dirname}/src/images`
+      }
+    },
+  ],
+}
