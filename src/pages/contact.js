e0d5916bca4ef76cbc9b0eabd37c2398a4373842
Conor Doyle <contact@conordoyle.io>
2018-07-12 17:12:36 +0100

    Scaffold out Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,131 @@
 import React from 'react';
 import styled from 'styled-components';
+import Img from 'gatsby-image';
 
-function Contact() {
+const Wrapper = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`;
+
+const H1 = styled.h1`
+  color: #004899;
+  margin-bottom: 0;
+`;
+const H2 = H1.withComponent('h2').extend`
+  margin: 4vw auto;
+`;
+
+const LightBlueSpan = styled.span`
+  color: #007fc8;
+`;
+
+const TextAndMapWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TextWrapper = styled.div`
+  h2:nth-child(1) {
+    margin-bottom: 0;
+  }
+  h2:nth-child(2) {
+    margin-top: 0;
+  }
+  div:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const Text = styled.p`
+  margin-bottom: 0;
+`;
+
+const Address = styled.div`
+  margin-bottom: 2vw;
+`;
+
+const ContactDetails = styled.div`
+  margin-bottom: 1vw;
+`;
+
+const MapImg = styled(Img)``;
+
+function Contact({ data }) {
   return (
-    <div>
-      Contact
-    </div>
+    <Wrapper>
+      <H1>Contact Us</H1>
+      <TextAndMapWrapper>
+        <TextWrapper>
+          <H2>Commercial Division</H2>
+          <H2>Co. Louth, Ireland</H2>
+
+          <Address>
+            <Text>
+              <b>Teva Pharmaceuticals</b>
+            </Text>
+            <Text>Finnabair Business Park, Dundalk,</Text>
+            <Text>Co. Louth, Ireland.</Text>
+          </Address>
+
+          <Text>
+            <LightBlueSpan>Freephone:</LightBlueSpan> 1800 - 201 700
+          </Text>
+          <Text>
+            <LightBlueSpan>Tel:</LightBlueSpan> 051-321740
+          </Text>
+          <Text>
+            <LightBlueSpan>Fax:</LightBlueSpan> 042 - 9351516
+          </Text>
+          <Text>
+            <LightBlueSpan>Email:</LightBlueSpan> info@teva.ie
+          </Text>
+
+          <H2>Retails Representatives</H2>
+
+          <ContactDetails>
+            <LightBlueSpan>Phil Osborn (Group accounts)</LightBlueSpan>
+            <Text>087 050 8779 - phil.osborn@teva.ie</Text>
+          </ContactDetails>
+          <ContactDetails>
+            <LightBlueSpan>Avril Ryan (South East)</LightBlueSpan>
+            <Text>087 223 4895 - avril.ryan@teva.ie</Text>
+          </ContactDetails>
+          <ContactDetails>
+            <LightBlueSpan>Jane Clements (West)</LightBlueSpan>
+            <Text>087 120 9148 - jane.clements@teva.ie</Text>
+          </ContactDetails>
+          <ContactDetails>
+            <LightBlueSpan>Paul Moran (Midlands)</LightBlueSpan>
+            <Text>087 799 2896 - paul.moran@teva.ie</Text>
+          </ContactDetails>
+          <ContactDetails>
+            <LightBlueSpan>Sinead Finucane (South West)</LightBlueSpan>
+            <Text>087 799 2858 - sinead.finucane@teva.ie</Text>
+          </ContactDetails>
+          <ContactDetails>
+            <LightBlueSpan>Collin Botha (North East)</LightBlueSpan>
+            <Text>087 668 5876 - collin.botha@teva.ie</Text>
+          </ContactDetails>
+        </TextWrapper>
+        <MapImg sizes={data.map.sizes} />
+      </TextAndMapWrapper>
+    </Wrapper>
   );
 }
+
+export const query = graphql`
+  query ContactQuery {
+    map: imageSharp(id: { regex: "/contact/map.png/" }) {
+      sizes(maxWidth: 676) {
+        src
+        srcSet
+        srcWebp
+        srcSetWebp
+        sizes
+        aspectRatio
+      }
+    }
+  }
+`;
 
 export default Contact;
