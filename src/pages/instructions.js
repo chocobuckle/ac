ddeee605ebc909372ab433aafd85cecb82a47681
Conor Doyle <contact@conordoyle.io>
2018-07-24 10:41:26 +0100

    Add GraphQL queries for 'Instructions' page

--- src/pages/instructions.js
+++ src/pages/instructions.js
@@ -1,11 +1,92 @@
 import React from 'react';
+import styled from 'styled-components';
+import Img from 'gatsby-image';
+import { ContentWrapper } from '../helpers/sharedStyles';
+import DownloadInstructionsForUse from '../components/DownloadInstructionsForUse';
+
 
 function Instructions() {
   return (
     <div>
-      Instructions
+      <DownloadInstructionsForUse
+        download={data.download}
+        bear={data.bear}
+        smallMask={data.smallMask}
+        largeMask={data.largeMask}
+      />
     </div>
   );
 }
+
+export const query = graphql`
+  query InstructionsQuery {
+    bpaAndDishwasherSafe: imageSharp(
+      id: { regex: "/overview/bpa_and_dishwasher_safe.png/" }
+    ) {
+      sizes(maxWidth: 749) {
+        src
+        srcSet
+        srcWebp
+        srcSetWebp
+        sizes
+        aspectRatio
+      }
+    }
+    macbook: imageSharp(id: { regex: "/misc/macbook_small.png/" }) {
+      sizes(maxWidth: 886) {
+        src
+        srcSet
+        srcWebp
+        srcSetWebp
+        sizes
+        aspectRatio
+      }
+    }
+    bear: imageSharp(id: { regex: "/overview/bear.png/" }) {
+      sizes(maxWidth: 1636) {
+        src
+        srcSet
+        srcWebp
+        srcSetWebp
+        sizes
+        aspectRatio
+      }
+    }
+    smallMask: imageSharp(
+      id: { regex: "/overview/small_mask_medium_mask.png/" }
+    ) {
+      sizes(maxWidth: 851) {
+        src
+        srcSet
+        srcWebp
+        srcSetWebp
+        sizes
+        aspectRatio
+      }
+    }
+    largeMask: imageSharp(
+      id: { regex: "/overview/large_mask_mouthpiece.png/" }
+    ) {
+      sizes(maxWidth: 851) {
+        src
+        srcSet
+        srcWebp
+        srcSetWebp
+        sizes
+        aspectRatio
+      }
+    }
+    download: imageSharp(id: { regex: "/overview/download.png/" }) {
+      sizes(maxWidth: 238) {
+        src
+        srcSet
+        srcWebp
+        srcSetWebp
+        sizes
+        aspectRatio
+      }
+    }
+  }
+`;
 
 export default Instructions;
