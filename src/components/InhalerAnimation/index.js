000589de19f80e6708fe217885137b144eda11b3
Conor Doyle <contact@conordoyle.io>
2018-07-26 13:25:45 +0100

    Add and position images for inhaler animation

--- src/components/InhalerAnimation/index.js
+++ src/components/InhalerAnimation/index.js
@@ -11,13 +11,26 @@ const ClickyThingNumber = styled.p`
   color: #fff;
   font-weight: 600;
   position: absolute;
-  left: 6vw;
-  top: 0.8vw;
 `;
 
 const clickyThingImgStyle = {
   maxWidth: '99px',
   width: '10vw'
+};
+
+const flipHorizontal = {
+  display: 'inline-block',
+  transform: 'scale(-1,1)'
+};
+
+const flipVertical = {
+  display: 'inline-block',
+  transform: 'scale(1,-1)'
+};
+
+const flipHorizontalAndVertical = {
+  display: 'inline-block',
+  transform: 'scale(-1,-1)'
 };
 
 class InhalerAnimation extends Component {
@@ -26,52 +39,115 @@ class InhalerAnimation extends Component {
   render() {
     const { animationClickyThing, inhalerAnimationImg } = this.props;
     return (
-      <ContentWrapper>
+      <ContentWrapper style={{ marginBottom: '-28.75vw' }}>
         <Img
           sizes={inhalerAnimationImg.sizes}
           style={{ maxWidth: '933px', width: '73vw', margin: '0 auto' }}
         />
-        <ClickyThingWrapper>
+        <ClickyThingWrapper
+          style={{
+            bottom: '28vw',
+            left: '1.5vw'
+          }}>
           <Img
             sizes={animationClickyThing.sizes}
-            style={{ ...clickyThingImgStyle }}
+            style={{
+              ...clickyThingImgStyle,
+              ...flipHorizontal
+            }}
           />
-          <ClickyThingNumber>1</ClickyThingNumber>
+          <ClickyThingNumber
+            style={{
+              left: '1.8vw',
+              top: '0.8vw'
+            }}>
+            1
+          </ClickyThingNumber>
         </ClickyThingWrapper>
-        <ClickyThingWrapper>
+        <ClickyThingWrapper
+          style={{
+            bottom: '46vw',
+            left: '34vw'
+          }}>
           <Img
             sizes={animationClickyThing.sizes}
             style={{ ...clickyThingImgStyle }}
           />
-          <ClickyThingNumber>2</ClickyThingNumber>
+          <ClickyThingNumber
+            style={{
+              left: '6vw',
+              top: '0.8vw'
+            }}>
+            2
+          </ClickyThingNumber>
         </ClickyThingWrapper>
-        <ClickyThingWrapper>
+        <ClickyThingWrapper
+          style={{
+            bottom: '48.5vw',
+            left: '42.5vw'
+          }}>
           <Img
             sizes={animationClickyThing.sizes}
             style={{ ...clickyThingImgStyle }}
           />
-          <ClickyThingNumber>3</ClickyThingNumber>
+          <ClickyThingNumber
+            style={{
+              left: '6vw',
+              top: '0.8vw'
+            }}>
+            3
+          </ClickyThingNumber>
         </ClickyThingWrapper>
-        <ClickyThingWrapper>
+        <ClickyThingWrapper
+          style={{
+            bottom: '48.5vw',
+            left: '80vw'
+          }}>
           <Img
             sizes={animationClickyThing.sizes}
             style={{ ...clickyThingImgStyle }}
           />
-          <ClickyThingNumber>4</ClickyThingNumber>
+          <ClickyThingNumber
+            style={{
+              left: '6vw',
+              top: '0.8vw'
+            }}>
+            4
+          </ClickyThingNumber>
         </ClickyThingWrapper>
-        <ClickyThingWrapper>
+        <ClickyThingWrapper
+          style={{
+            bottom: '33.5vw',
+            left: '65vw'
+          }}>
           <Img
             sizes={animationClickyThing.sizes}
-            style={{ ...clickyThingImgStyle }}
+            style={{ ...clickyThingImgStyle, ...flipVertical }}
           />
-          <ClickyThingNumber>5</ClickyThingNumber>
+          <ClickyThingNumber
+            style={{
+              left: '6vw',
+              top: '0.8vw'
+            }}>
+            5
+          </ClickyThingNumber>
         </ClickyThingWrapper>
-        <ClickyThingWrapper>
+        <ClickyThingWrapper
+          style={{
+            bottom: '39.5vw',
+            left: '42vw'
+          }}>
           <Img
             sizes={animationClickyThing.sizes}
-            style={{ ...clickyThingImgStyle }}
+            style={{ ...clickyThingImgStyle, ...flipHorizontalAndVertical }}
           />
-          <ClickyThingNumber>6</ClickyThingNumber>
+          <ClickyThingNumber
+            style={{
+              left: '2vw',
+              top: '0.8vw'
+            }}>
+            6
+          </ClickyThingNumber>
         </ClickyThingWrapper>
       </ContentWrapper>
     );
