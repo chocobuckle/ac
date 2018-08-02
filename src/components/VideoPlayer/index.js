12b7cfa7f7a16be22974bd4eedd8149ec00aa1e5
Conor Doyle <contact@conordoyle.io>
2018-08-02 16:32:03 +0100

    Macbook on Home page style fixed

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -31,7 +31,7 @@ function VideoPlayer({ activeVideoURL, handleVideoLoading }) {
         style={{
           position: 'absolute',
           left: 0,
-          bottom: activeVideoURL ? '24.575vw' : '20.625vw'
+          bottom: activeVideoURL ? '58.9%' : '20.625vw'
         }}
       />
     </VideoPlayerWrapper>
