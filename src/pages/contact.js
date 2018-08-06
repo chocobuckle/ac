f029de3661adc4394e5f7542ae5890fe3b897f70
Conor Doyle <contact@conordoyle.io>
2018-08-06 19:45:42 +0100

    Latest

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,6 +10,31 @@ import {
   FlexColumnWrapper
 } from 'helpers/sharedStyles';
 
+const LocalH1 = H1.extend`
+  margin-top: 29vw;
+
+  @media screen and (min-width: 600px) {
+    font-size: 46.5px;
+  }
+
+  @media screen and (min-width: 996px) {
+    font-size: 46.5px;
+    margin-top: 288.84px;
+  }
+`;
+
+const LocalH2 = H2.extend`
+  @media screen and (min-width: 600px) {
+    font-size: 31.8px;
+  }
+`;
+
+const LocalText = Text.extend`
+  @media screen and (min-width: 600px) {
+    font-size: 23.4px;
+  }
+`;
+
 const Address = styled.div`
   margin: 0.8em 0;
   p {
@@ -30,63 +55,70 @@ const Gradient = styled.div`
   z-index: 1;
 `;
 
+const MapImg = styled(Img)`
+  margin: 0 auto;
+  @media screen and (min-width: 600px) {
+    width: 546px;
+  }
+`;
+
 function Contact({ data }) {
   return (
     <Gradient>
       <ContentWrapper style={{ paddingBottom: 0 }}>
-        <H1 style={{ marginTop: '3.75em' }}>Contact Us</H1>
+        <LocalH1>Contact Us</LocalH1>
         <FlexColumnWrapper>
-          <H2 style={{ marginBottom: 0 }}>Commercial Division</H2>
-          <H2>Co. Louth, Ireland</H2>
+          <LocalH2 style={{ marginBottom: 0 }}>Commercial Division</LocalH2>
+          <LocalH2>Co. Louth, Ireland</LocalH2>
 
           <Address>
-            <Text>
+            <LocalText>
               <b>Teva Pharmaceuticals</b>
-            </Text>
-            <Text>Finnabair Business Park, Dundalk,</Text>
-            <Text>Co. Louth, Ireland.</Text>
+            </LocalText>
+            <LocalText>Finnabair Business Park, Dundalk,</LocalText>
+            <LocalText>Co. Louth, Ireland.</LocalText>
           </Address>
 
-          <Text>
+          <LocalText>
             <LightBlueSpan>Freephone:</LightBlueSpan> 1800 - 201 700
-          </Text>
-          <Text>
+          </LocalText>
+          <LocalText>
             <LightBlueSpan>Tel:</LightBlueSpan> 051 - 321740
-          </Text>
-          <Text>
+          </LocalText>
+          <LocalText>
             <LightBlueSpan>Fax:</LightBlueSpan> 042 - 9351516
-          </Text>
-          <Text style={{ marginBottom: 0 }}>
+          </LocalText>
+          <LocalText style={{ marginBottom: 0 }}>
             <LightBlueSpan>Email:</LightBlueSpan> info@teva.ie
-          </Text>
+          </LocalText>
 
-          <H2 style={{ margin: '0.75em 0' }}>Retails Representatives</H2>
+          <LocalH2 style={{ margin: '0.75em 0' }}>Retails Representatives</LocalH2>
 
-          <Text>
+          <LocalText>
             <LightBlueSpan>Phil Osborn (Group accounts)</LightBlueSpan>
             <br />087 050 8779 - phil.osborn@teva.ie
-          </Text>
-          <Text>
+          </LocalText>
+          <LocalText>
             <LightBlueSpan>Avril Ryan (South East)</LightBlueSpan>
             <br />087 223 4895 - avril.ryan@teva.ie
-          </Text>
-          <Text>
+          </LocalText>
+          <LocalText>
             <LightBlueSpan>Jane Clements (West)</LightBlueSpan>
             <br />087 120 9148 - jane.clements@teva.ie
-          </Text>
-          <Text>
+          </LocalText>
+          <LocalText>
             <LightBlueSpan>Paul Moran (Midlands)</LightBlueSpan>
             <br />087 799 2896 - paul.moran@teva.ie
-          </Text>
-          <Text>
+          </LocalText>
+          <LocalText>
             <LightBlueSpan>Sinead Finucane (South West)</LightBlueSpan>
             <br />087 799 2858 - sinead.finucane@teva.ie
-          </Text>
-          <Text style={{ marginBottom: 0 }}>
+          </LocalText>
+          <LocalText style={{ marginBottom: 0 }}>
             <LightBlueSpan>Collin Botha (North East)</LightBlueSpan>
             <br />087 668 5876 - collin.botha@teva.ie
-          </Text>
-          <Img sizes={data.map.sizes} />
+          </LocalText>
+          <MapImg sizes={data.map.sizes} />
         </FlexColumnWrapper>
       </ContentWrapper>
     </Gradient>
